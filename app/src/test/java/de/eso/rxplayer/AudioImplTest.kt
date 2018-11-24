6624f0a4782085f09ed906748f7cef43f949e855
Yuriy Kulikov <yuriy.kulikov@esolutions.de>
2018-11-24 14:25:07 +0100

    Adjusted the test

--- app/src/test/java/de/eso/rxplayer/AudioImplTest.kt
+++ app/src/test/java/de/eso/rxplayer/AudioImplTest.kt
@@ -1,8 +1,7 @@
 package de.eso.rxplayer
 
 import io.reactivex.schedulers.TestScheduler
-import org.assertj.core.api.AbstractListAssert
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 /**
@@ -11,18 +10,14 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class AudioImplTest {
-    val scheduler = TestScheduler()
-    val audio = AudioImpl(scheduler)
+    private val scheduler = TestScheduler()
+    private val audio = AudioImpl(scheduler)
 
     @Test
     fun `all connections are initially stopped`() {
         Audio.Connection.values()
                 .map { audio.observe(it).blockingFirst() }
-                .assertThatIt()
+                .let { assertThat(it) }
                 .containsOnly(Audio.AudioState.STOPPED)
-    }
-
-    fun <T> List<T>.assertThatIt(): AbstractListAssert<*, out MutableList<out T>, T> {
-        return Assertions.assertThat(this)
     }
 }
